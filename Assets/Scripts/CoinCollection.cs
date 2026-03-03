be56d0b8e33bc5865a0991a03447ecd20235b11f
Zhe <alannayou.z@gmail.com>
2026-03-02 21:21:27 -0500

    Downloaded packages for multiplayer
    I have downloaded network for gameobjects and multiplayer play mode on packet manager

--- Assets/Scripts/CoinCollection.cs
+++ Assets/Scripts/CoinCollection.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 // Step 1: Basically player collide, coin destroys, counter + 1; -> finished
 // Step 2: Change Terrain + make player able to jump 
-// Step 3: random assortment of coin order scattered on map
-// Step 4: Implement multi-player by 8 - 9 pm
+// Step 3: random assortment of coin order scattered on map -> finished
+// Step 4: Implement multi-player by 9 - 10 pm working
 // step 5: Write up 9 - 10 pm
 // step 6: Light mechanism?? Basically each player emits light, more player = wider range of vision?? 11-11:30
 // Step 7: Sumbit at 11:58
